e6e8fd469be7177845b18a272025991c61f8eec9
barabull <afdsjbkdv@mail.ru>
2022-02-20 23:04:48 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        //TASK_b
+        char ch = 'h';
+        int i = 255;
+        short s = 15;
+        byte v = 6;
+        long l = 100;
+        boolean b = true;
+        double d = 5.9;
+        String str = "hello";
+    }
+}
